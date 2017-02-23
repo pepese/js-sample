f2b2da55b4acd43bcf076406377632dfec6c032f
pepese <tanakakns@tanakaknsnoMacBook-Pro.local>
2017-02-23 22:38:07 +0900

    update

--- angular-sample/src/app/app.component.ts
+++ angular-sample/src/app/app.component.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
-import '../../public/css/styles.css';
+
 @Component({
   selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `<h1>Hello {{name}}</h1>`
 })
-export class AppComponent { }
+export class AppComponent { name = 'Angular'; }
