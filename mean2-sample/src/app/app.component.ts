71cc51f4db6f28889549edc30d29fe367162a2b1
pepese <tanakakns@tanakakensukenoMacBook-Pro.local>
2017-03-18 18:02:45 +0900

    update

--- mean2-sample/src/app/app.component.ts
+++ mean2-sample/src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { Http, Response } from '@angular/http';
+
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
 
 import { SampleService } from './sample/sample.service'
 
@@ -11,10 +16,22 @@ import { SampleService } from './sample/sample.service'
 export class AppComponent {
   title: string;
   text: string;
+  resText: string;
 
   constructor(
-    private sampleService: SampleService
+    private sampleService: SampleService,
+    private http: Http
   ){
     this.title = sampleService.getTitle();
+  }
+
+  getSample() {
+    //return this.http.get("http://localhost:3000/api")
+    //                .map(res => res.toString())
+    //                .subscribe(t => this.resText = t);
+    return this.http.get("https://httpbin.org/ip")
+                    //.subscribe(res => console.log(res));
+                    .map(res => res.json())
+                    .subscribe(t => this.resText = t.origin);
   }
 }
