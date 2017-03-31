9c1f8fd02b00cec01b779582c4eee381eebe7a29
pepese <tanakakns@tanakaknsnoMacBook-Pro.local>
2017-03-31 15:27:56 +0900

    update

--- line-chatbot/routes/index.js
+++ line-chatbot/routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const request = require('request');
+const crypto = require("crypto");
 
 const path = require('path');
 const env = process.env.NODE_ENV || "development";
@@ -8,10 +9,22 @@ const config = require(path.join(__dirname, '../config', 'config.json'))[env];
 
 const lineUrl = config.lineUrl;
 const lineChannelAccessToken = config.lineChannelAccessToken;
+const lineChannelSecret = config.lineChannelSecret;
+
+// signatureのvalidation
+const validate_signature = function(signature, body) {
+  return signature == crypto.createHmac('sha256', LINE_CHANNEL_SECRET).update(new Buffer(JSON.stringify(body), 'utf8')).digest('base64');
+};
 
 router.post('/', (req, res) => {
   //console.log("Webhook Accepted !");
   //console.log(req.body);
+
+  // signatureのvalidation
+  if (!validate_signature(req.headers['x-line-signature'], req.body)) {
+    res.status(401);
+    return null;
+  }
 
   let webhookEventObject = req.body.events[0];
   //メッセージが送られて来た場合
