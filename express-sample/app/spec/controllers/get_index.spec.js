b83ac2b6366a2c34fe56e33e7d9cb309b18f5a1b
pepese <tanakakns@tanakaknsnoMacBook-Pro.local>
2017-08-15 21:29:35 +0900

    update

--- express-sample/app/spec/controllers/get_index.spec.js
+++ express-sample/app/spec/controllers/get_index.spec.js
@@ -1,15 +1,11 @@
-var sinon = require('sinon');
-var chai = require('chai');
-var expect = chai.expect;
-var should = require('should');
+const sinon = require('sinon');
+const chai = require('chai');
+const expect = chai.expect;
+const should = require('should');
 
-var get_index = require('../../controllers/get_index');
+const get_index = require('../../controllers/get_index');
 
-//var request = require('supertest');
-//var app = require('../../app');
-//var req = request(app);
-
-describe('get_index', function() {
+describe('get_index', () => {
   it('Index画面が1回レンダリングされること', function(done) {
     var req,res,spy;
     req = res = {};
@@ -20,14 +16,4 @@ describe('get_index', function() {
 
     done();
   });
-//  it('「/」でアクセスできること', function() {
-//    req.get('/').end(function(err, ret) {
-//      var res = ret.res;
-//      var statusCode = res.statusCode;
-//      var text = res.text;
-//      statusCode.should.equal(200);
-//      text.should.equal('respond with a resource');
-//      done();
-//    });
-//  });
 });
